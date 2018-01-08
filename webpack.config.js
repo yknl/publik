5fa331a2ed943b83e73d2d8b74c24b51feb8d89c
Ubuntu <larry@blockstack-explorer.wvqmxzznymcubjivhurhh2jczg.ex.internal.cloudapp.net>
2018-01-08 19:41:30 +0000

    Update webpack settings.

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,23 @@ const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ManifestAssetPlugin = new CopyWebpackPlugin([ { from: 'src/assets/manifest.json', to: 'manifest.json' } ]);
 const IconAssetPlugin = new CopyWebpackPlugin([ { from: 'src/images/icon-192x192.png', to: 'icon-192x192.png' } ]);
+const UglifyEsPlugin = require('uglify-es-webpack-plugin');
+const UglifyEsPluginConfig = new UglifyEsPlugin({
+	mangle: {
+		reserved: [
+                	'Buffer',
+                        'BigInteger',
+                        'Point',
+                        'ECPubKey',
+                        'ECKey',
+                        'sha512_asm',
+                        'asm',
+                        'ECPair',
+                        'HDNode'
+                ]
+        }
+})
+
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
@@ -44,5 +61,10 @@ module.exports = {
       { test: /\.css$/, loader: 'style-loader!css-loader' }
     ]
   },
-  plugins: [HtmlWebpackPluginConfig, ManifestAssetPlugin, IconAssetPlugin]
+  plugins: [
+	HtmlWebpackPluginConfig, 
+	ManifestAssetPlugin, 
+	IconAssetPlugin,
+	UglifyEsPluginConfig
+ ]
 }
